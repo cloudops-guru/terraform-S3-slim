aa4fbbe5e526784ae04df7c5834c61b4b808637f
Nikhil Surendran <snikhil@levi.com>
2021-07-28 11:18:10 +0530

    Updated main

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
-resource "aws_s3_bucket" "s3_bucket" {
+locals {
+  attach_policy = var.attach_elb_log_delivery_policy || var.attach_lb_log_delivery_policy || var.attach_deny_insecure_transport_policy || var.attach_policy
+}
 
+resource "aws_s3_bucket" "this" {
   count = var.create_bucket ? 1 : 0
 
-  bucket              = var.bucket
-  bucket_prefix       = var.bucket_prefix
-  acl                 = var.acl
+  bucket = var.bucket
+  bucket_prefix = var.bucket_prefix 
+  acl = var.acl != "null" ? var.acl : null
+
   force_destroy       = var.force_destroy
   acceleration_status = var.acceleration_status
+  request_payer       = var.request_payer
+
   tags =  tomap({
     "Name"           = var.bucket
   }) 
@@ -15,8 +21,10 @@ resource "aws_s3_bucket" "s3_bucket" {
     for_each = length(keys(var.website)) == 0 ? [] : [var.website]
 
     content {
-    index_document = lookup(website.value, "index_document", null)
-    error_document = lookup(website.value, "error_document", null)
+      index_document           = lookup(website.value, "index_document", null)
+      error_document           = lookup(website.value, "error_document", null)
+      redirect_all_requests_to = lookup(website.value, "redirect_all_requests_to", null)
+      routing_rules            = lookup(website.value, "routing_rules", null)
     }
   }
 
@@ -50,8 +58,19 @@ resource "aws_s3_bucket" "s3_bucket" {
     }
   }
 
+  dynamic "grant" {
+    for_each = try(jsondecode(var.grant), var.grant)
+
+    content {
+      id          = lookup(grant.value, "id", null)
+      type        = grant.value.type
+      permissions = grant.value.permissions
+      uri         = lookup(grant.value, "uri", null)
+    }
+  }
+
   dynamic "lifecycle_rule" {
-    for_each = var.lifecycle_rule
+    for_each = try(jsondecode(var.lifecycle_rule), var.lifecycle_rule)
 
     content {
       id                                     = lookup(lifecycle_rule.value, "id", null)
@@ -60,6 +79,7 @@ resource "aws_s3_bucket" "s3_bucket" {
       abort_incomplete_multipart_upload_days = lookup(lifecycle_rule.value, "abort_incomplete_multipart_upload_days", null)
       enabled                                = lifecycle_rule.value.enabled
 
+      # Max 1 block - expiration
       dynamic "expiration" {
         for_each = length(keys(lookup(lifecycle_rule.value, "expiration", {}))) == 0 ? [] : [lookup(lifecycle_rule.value, "expiration", {})]
 
@@ -70,6 +90,7 @@ resource "aws_s3_bucket" "s3_bucket" {
         }
       }
 
+      # Several blocks - transition
       dynamic "transition" {
         for_each = lookup(lifecycle_rule.value, "transition", [])
 
@@ -79,9 +100,94 @@ resource "aws_s3_bucket" "s3_bucket" {
           storage_class = transition.value.storage_class
         }
       }
+
+      # Max 1 block - noncurrent_version_expiration
+      dynamic "noncurrent_version_expiration" {
+        for_each = length(keys(lookup(lifecycle_rule.value, "noncurrent_version_expiration", {}))) == 0 ? [] : [lookup(lifecycle_rule.value, "noncurrent_version_expiration", {})]
+
+        content {
+          days = lookup(noncurrent_version_expiration.value, "days", null)
+        }
+      }
+
+      # Several blocks - noncurrent_version_transition
+      dynamic "noncurrent_version_transition" {
+        for_each = lookup(lifecycle_rule.value, "noncurrent_version_transition", [])
+
+        content {
+          days          = lookup(noncurrent_version_transition.value, "days", null)
+          storage_class = noncurrent_version_transition.value.storage_class
+        }
+      }
     }
   }
 
+  # Max 1 block - replication_configuration
+  dynamic "replication_configuration" {
+    for_each = length(keys(var.replication_configuration)) == 0 ? [] : [var.replication_configuration]
+
+    content {
+      role = replication_configuration.value.role
+
+      dynamic "rules" {
+        for_each = replication_configuration.value.rules
+
+        content {
+          id       = lookup(rules.value, "id", null)
+          priority = lookup(rules.value, "priority", null)
+          prefix   = lookup(rules.value, "prefix", null)
+          status   = rules.value.status
+
+          dynamic "destination" {
+            for_each = length(keys(lookup(rules.value, "destination", {}))) == 0 ? [] : [lookup(rules.value, "destination", {})]
+
+            content {
+              bucket             = destination.value.bucket
+              storage_class      = lookup(destination.value, "storage_class", null)
+              replica_kms_key_id = lookup(destination.value, "replica_kms_key_id", null)
+              account_id         = lookup(destination.value, "account_id", null)
+
+              dynamic "access_control_translation" {
+                for_each = length(keys(lookup(destination.value, "access_control_translation", {}))) == 0 ? [] : [lookup(destination.value, "access_control_translation", {})]
+
+                content {
+                  owner = access_control_translation.value.owner
+                }
+              }
+            }
+          }
+
+          dynamic "source_selection_criteria" {
+            for_each = length(keys(lookup(rules.value, "source_selection_criteria", {}))) == 0 ? [] : [lookup(rules.value, "source_selection_criteria", {})]
+
+            content {
+
+              dynamic "sse_kms_encrypted_objects" {
+                for_each = length(keys(lookup(source_selection_criteria.value, "sse_kms_encrypted_objects", {}))) == 0 ? [] : [lookup(source_selection_criteria.value, "sse_kms_encrypted_objects", {})]
+
+                content {
+
+                  enabled = sse_kms_encrypted_objects.value.enabled
+                }
+              }
+            }
+          }
+
+          dynamic "filter" {
+            for_each = length(keys(lookup(rules.value, "filter", {}))) == 0 ? [] : [lookup(rules.value, "filter", {})]
+
+            content {
+              prefix = lookup(filter.value, "prefix", null)
+              tags   = lookup(filter.value, "tags", null)
+            }
+          }
+
+        }
+      }
+    }
+  }
+
+  # Max 1 block - server_side_encryption_configuration
   dynamic "server_side_encryption_configuration" {
     for_each = length(keys(var.server_side_encryption_configuration)) == 0 ? [] : [var.server_side_encryption_configuration]
 
@@ -91,6 +197,7 @@ resource "aws_s3_bucket" "s3_bucket" {
         for_each = length(keys(lookup(server_side_encryption_configuration.value, "rule", {}))) == 0 ? [] : [lookup(server_side_encryption_configuration.value, "rule", {})]
 
         content {
+          bucket_key_enabled = lookup(rule.value, "bucket_key_enabled", null)
 
           dynamic "apply_server_side_encryption_by_default" {
             for_each = length(keys(lookup(rule.value, "apply_server_side_encryption_by_default", {}))) == 0 ? [] : [
@@ -105,41 +212,167 @@ resource "aws_s3_bucket" "s3_bucket" {
       }
     }
   }
+
+  # Max 1 block - object_lock_configuration
+  dynamic "object_lock_configuration" {
+    for_each = length(keys(var.object_lock_configuration)) == 0 ? [] : [var.object_lock_configuration]
+
+    content {
+      object_lock_enabled = object_lock_configuration.value.object_lock_enabled
+
+      dynamic "rule" {
+        for_each = length(keys(lookup(object_lock_configuration.value, "rule", {}))) == 0 ? [] : [lookup(object_lock_configuration.value, "rule", {})]
+
+        content {
+          default_retention {
+            mode  = lookup(lookup(rule.value, "default_retention", {}), "mode")
+            days  = lookup(lookup(rule.value, "default_retention", {}), "days", null)
+            years = lookup(lookup(rule.value, "default_retention", {}), "years", null)
+          }
+        }
+      }
+    }
+  }
+
 }
 
-resource "aws_s3_bucket_public_access_block" "publicAccess" {
-  count = var.block_public_access? 1 : 0
-  bucket = aws_s3_bucket.s3_bucket[0].id
+resource "aws_s3_bucket_policy" "this" {
+  count = var.create_bucket && local.attach_policy ? 1 : 0
 
-  block_public_acls   = var.block_public_acls
-  block_public_policy = var.block_public_policy
-  ignore_public_acls = var.ignore_public_acls
-  restrict_public_buckets = var.restrict_public_buckets
+  bucket = aws_s3_bucket.this[0].id
+  policy = data.aws_iam_policy_document.combined[0].json
 }
 
-locals {
-   a3_bucket_policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "MYBUCKETPOLICY"
-    Statement = [
-      {
-        Sid       = "S3CustomPolicy"
-        Effect    = "Deny" 
-        "Action": [
-          "s3:ListBucket",
-          "s3:ListObject",
-          "s3:GetObject"  
-        ],
-        Resource = [
-          aws_s3_bucket.s3_bucket[0].arn,
-          "${aws_s3_bucket.s3_bucket[0].arn}/*",
-        ] 
-      },
+data "aws_iam_policy_document" "combined" {
+  count = var.create_bucket && local.attach_policy ? 1 : 0
+
+  source_policy_documents = compact([
+    var.attach_elb_log_delivery_policy ? data.aws_iam_policy_document.elb_log_delivery[0].json : "",
+    var.attach_lb_log_delivery_policy ? data.aws_iam_policy_document.lb_log_delivery[0].json : "",
+    var.attach_deny_insecure_transport_policy ? data.aws_iam_policy_document.deny_insecure_transport[0].json : "",
+    var.attach_policy ? var.policy : ""
+  ])
+}
+
+# AWS Load Balancer access log delivery policy
+data "aws_elb_service_account" "this" {
+  count = var.create_bucket && var.attach_elb_log_delivery_policy ? 1 : 0
+}
+
+data "aws_iam_policy_document" "elb_log_delivery" {
+  count = var.create_bucket && var.attach_elb_log_delivery_policy ? 1 : 0
+
+  statement {
+    sid = ""
+
+    principals {
+      type        = "AWS"
+      identifiers = data.aws_elb_service_account.this.*.arn
+    }
+
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
     ]
-  })
-} 
 
-resource "aws_s3_bucket_policy" "s3_bucket_policy" {
-  bucket = aws_s3_bucket.s3_bucket[0].id 
-  policy =  (var.aws_s3_policy  != "") ? var.aws_s3_policy : local.a3_bucket_policy
+    resources = [
+      "${aws_s3_bucket.this[0].arn}/*",
+    ]
+  }
+}
+
+# ALB/NLB
+
+data "aws_iam_policy_document" "lb_log_delivery" {
+  count = var.create_bucket && var.attach_lb_log_delivery_policy ? 1 : 0
+
+  statement {
+    sid = "AWSLogDeliveryWrite"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.this[0].arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+
+  statement {
+    sid = "AWSLogDeliveryAclCheck"
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetBucketAcl",
+    ]
+
+    resources = [
+      aws_s3_bucket.this[0].arn,
+    ]
+
+  }
+}
+
+data "aws_iam_policy_document" "deny_insecure_transport" {
+  count = var.create_bucket && var.attach_deny_insecure_transport_policy ? 1 : 0
+
+  statement {
+    sid    = "denyInsecureTransport"
+    effect = "Deny"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      aws_s3_bucket.this[0].arn,
+      "${aws_s3_bucket.this[0].arn}/*",
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values = [
+        "false"
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  count = var.create_bucket && var.attach_public_policy ? 1 : 0
+
+  # Chain resources (s3_bucket -> s3_bucket_policy -> s3_bucket_public_access_block)
+  # to prevent "A conflicting conditional operation is currently in progress against this resource."
+  bucket = local.attach_policy ? aws_s3_bucket_policy.this[0].id : aws_s3_bucket.this[0].id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
 }
